e68aef95422b9a8d44a709767ade2bb313ebb26f
Lorenzo Fiestas <hik.lorenzo@hotmail.com>
2024-09-28 16:37:33 +0300

    Finished with print_signal

--- tests/tests.c
+++ tests/tests.c
@@ -32,8 +32,12 @@ static void dft(double dft_re[], double dft_im[], const double xs[], size_t n)
 //     }
 // }
 
+// Simple testing filter, window_size is expected to be small
 void moving_average(double ys[], const double xs[], size_t length, size_t window_size)
 {
+    gp_assert(window_size & 1);
+    gp_assert(window_size >= 3, "%zu", window_size);
+
     // Extrapolate beginning
     for (int i = 0; i < (int)window_size/2; ++i) {
         double sum = 0.;
@@ -65,12 +69,13 @@ void moving_average(double ys[], const double xs[], size_t length, size_t window
     }
 }
 
-void print_signal(double signal[], size_t length)
+void print_signal(const char* name, double signal[], size_t length)
 {
     #define HEIGHT 15 // should be odd
     #define WIDTH 100
 
     static double* interpolated = NULL;
+    static double* filtered     = NULL;
 
     char graphic[HEIGHT][WIDTH];
     memset(graphic, ' ', sizeof graphic);
@@ -89,7 +94,7 @@ void print_signal(double signal[], size_t length)
             interpolated_length *= 2;
         size_t k = interpolated_length/length;
 
-        interpolated = realloc(interpolated, interpolated_length * sizeof interpolated[0]);
+        interpolated = realloc(interpolated, 2 * interpolated_length * sizeof interpolated[0]);
         for (size_t i = 0; i < length - 1; ++i) {
             for (size_t j = 0; j < k; ++j)
                 interpolated[k*i + j] = (1. - (double)j/k)*signal[i] + ((double)j/k)*signal[i + 1];
@@ -101,19 +106,20 @@ void print_signal(double signal[], size_t length)
         signal = interpolated;
         length = interpolated_length;
     }
-    // Draw graphic
-    size_t i = 0;
-    for (size_t x = 0; x < WIDTH; ++x) {
-        double val = 0.;
 
-        if (length == WIDTH)
-            val = signal[x];
-        else { // decimate
-            for (; i < (x + 1)*length/WIDTH; ++i)
-                val += signal[i];
-            size_t interval = (x + 1)*length/WIDTH - x*length/WIDTH;
-            val /= interval;
-        }
+    if (length != WIDTH) { // filter noise for decimation
+        size_t window_size = 2*length/WIDTH;
+        window_size |= (window_size + 1) & 1; // make odd
+        filtered = realloc(filtered, length * sizeof filtered[0]);
+        moving_average(filtered, signal, length, window_size);
+        signal = filtered;
+    }
+    // Draw graphic
+    for (size_t x = 0; x < WIDTH; ++x) {
+        double val = length != WIDTH ?
+            signal[x*length/WIDTH] // decimate
+          : signal[x];
+
         if (val == 1.)
             graphic[0][x] = '+';
         else if (val > 1.)
@@ -126,7 +132,7 @@ void print_signal(double signal[], size_t length)
         }
     }
     // Render
-    puts("");
+    puts(name);
     for (size_t y = 0; y < HEIGHT; ++y) {
         for (size_t x = 0; x < WIDTH; ++x) {
             putchar(graphic[y][x]);
@@ -138,18 +144,6 @@ void print_signal(double signal[], size_t length)
 
 int main(void)
 {
-    double xs[100];
-    double ys[100];
-    size_t xs_length = sizeof xs/sizeof xs[0];
-    for (size_t i = 0; i < xs_length; ++i) {
-        //xs[i] = sin(2*FFT_PI*i/xs_length);
-        xs[i] = i < xs_length / 2 ? 1. : -1.;
-    }
-    print_signal(xs, xs_length);
-    moving_average(ys, xs, xs_length, 5);
-    print_signal(ys, xs_length);
-    return 0;
-
     gp_suite("FFT"); {
         gp_test("Bit reversal sort"); {
             double res[8]    = { 0, 1, 2, 3, 4, 5, 6, 7 };
@@ -173,15 +167,15 @@ int main(void)
             dft(dft_re, dft_im, fft_re, SIZE);
             fft_complex(fft_re, fft_im, SIZE);
 
-            print_signal(dft_re, SIZE);
-            print_signal(dft_im, SIZE);
-            print_signal(fft_re, SIZE);
-            print_signal(fft_im, SIZE);
+            print_signal("dft_re", dft_re, SIZE);
+            print_signal("dft_im", dft_im, SIZE);
+            print_signal("fft_re", fft_re, SIZE);
+            print_signal("fft_im", fft_im, SIZE);
 
-            // for (size_t i = 0; i < SIZE; ++i) {
-            //     gp_assert(gp_fapprox(fft_re[i], dft_re[i], 0.00001), "%zu", i, "%g", fft_re[i], "%g", dft_re[i]);
-            //     gp_assert(gp_fapprox(fft_im[i], dft_im[i], 0.00001), "%zu", i, "%g", fft_im[i], "%g", dft_im[i]);
-            // }
+            for (size_t i = 0; i < SIZE; ++i) {
+                gp_assert(gp_fapprox(fft_re[i], dft_re[i], 0.00001), "%zu", i, "%g", fft_re[i], "%g", dft_re[i]);
+                gp_assert(gp_fapprox(fft_im[i], dft_im[i], 0.00001), "%zu", i, "%g", fft_im[i], "%g", dft_im[i]);
+            }
         }
     } // gp_suite("FFT")
 }
